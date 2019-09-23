16705e611f50426a4ef44d71bb47cb42f1bc3f60
Christopher Lemke <spcbrn1@gmail.com>
2019-09-23 14:30:28 -0600

    feat: update prototype functions to be more optimized, fix canvas render example

--- Engine/GameOfLife.js
+++ Engine/GameOfLife.js
@@ -12,38 +12,38 @@ class GameOfLife extends GameCore {
 
     this.width = width || 50;
     this.height = height || 50;
-    
+
     this.grid = null;
     this.gridBinary = null;
     this.useRandomGrid = random || false;
-    
+
     seed_frame instanceof Array &&
-    seed_frame[0] instanceof Array &&
-    typeof seed_frame[0][0] === "number" &&
+      seed_frame[0] instanceof Array &&
+      typeof seed_frame[0][0] === 'number' &&
       (() => {
         this.gridBinary = seed_frame;
         this.height = seed_frame.length;
         this.width = seed_frame[0].length;
       })();
-    
+
     this.bioMode = bio_mode || false;
-    
+
     this.dutyCycle = [];
     this.frameRegister = [];
-    
+
     this.interval = null;
     this._to = null;
     this.framesElapsed = 0;
     this.useRequestAnimationFrame = use_raf || true;
     this.render = ts => render(this.gridBinary, this.framesElapsed, ts);
-      console.log('whai')
+
     this._populateGameGrid();
   }
 }
 
 /*---------------------- RUNNING THE GAME ----------------------*/
 
-GameOfLife.prototype._initializeGame = function(interval, frame_limit = 0) {
+GameOfLife.prototype.initializeGame = function(interval, frame_limit = 0) {
   this.interval = interval || 200;
 
   this.useRequestAnimationFrame
@@ -76,14 +76,10 @@ GameOfLife.prototype._subscribeCellToDutyCycle = function(callback) {
 
 GameOfLife.prototype._dispatchRunDutyCycle = function() {
   // prettier-ignore
-  this.dutyCycle = this.dutyCycle.filter(distributeLifeForce =>
-    typeof distributeLifeForce === "function"
-      ? (() => {
-        distributeLifeForce();
-        return false;
-      })()
-      : false
-  );
+  this.dutyCycle = this.dutyCycle.filter(distributeLifeForce => {
+    if (typeof distributeLifeForce === "function") distributeLifeForce();
+    return false;
+  });
 };
 
 /*---------------------- FRAME REGISTER ----------------------*/
@@ -94,14 +90,11 @@ GameOfLife.prototype._subscribeCellToFrameRegister = function(callback) {
 
 GameOfLife.prototype._dispatchApplyRulesToRegister = function() {
   // prettier-ignore
-  this.frameRegister = this.frameRegister.filter(applyRulesToCell =>
-    typeof applyRulesToCell === "function"
-      ? (() => {
-        applyRulesToCell((coords, living_next) => {
-          this.gridBinary[coords[1]][coords[0]] = living_next;
-        });
-        return false;
-      })()
-      : false
-  );
+  this.frameRegister = this.frameRegister.filter(applyRulesToCell => {
+    if (typeof applyRulesToCell === "function")
+      applyRulesToCell((coords, living_next) => {
+        this.gridBinary[coords[1]][coords[0]] = living_next;
+      });
+    return false;
+  });
 };
